8606fa31a11e56544084a1b8004a07b1e4eee08f
Václav Chlumský <chlumskyvaclav@gmail.com>
2022-11-06 12:34:48 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# WORK IN PROGRESS
+
+# nano-lights
+![screenshot](https://github.com/vchlum/nano-lights/blob/main/screenshot.png)
+
+## Gnome Shell extension
+nano-lights is a Gnome Shell extension for controlling Nanoleaf lights on local network.
+
+
+## Supported Gnome Shell version
+This extension supports Gnome Shell verison 40 and above.
+
+## Manual installation
+
+ 1. `git clone https://github.com/vchlum/nano-lights.git`
+ 1. `cd nano-lights`
+ 1. `./release.sh`
+ 1. `gnome-extensions install nano-lights@chlumskyvaclav.gmail.com.zip`
+ 1. Log out & Log in
+ 1. `gnome-extensions enable nano-lights@chlumskyvaclav.gmail.com`
---
+++ extension.js
@@ -0,0 +1,72 @@
+'use strict';
+
+/**
+ * extension nano-lights
+ * JavaScript Gnome extension for Nanoleaf devices.
+ *
+ * @author Václav Chlumský
+ * @copyright Copyright 2022, Václav Chlumský.
+ */
+
+ /**
+ * @license
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 Václav Chlumský
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const Main = imports.ui.main;
+
+/**
+ * This function is called once the extension is loaded, not enabled.
+ *
+ * @method init
+ */
+function init() {
+
+    ExtensionUtils.initTranslations();
+}
+
+/**
+ * This function could be called after the extension is enabled.
+ *
+ * @method enable
+ */
+function enable() {
+
+    nanoLightsMenu = new NanoMenu.NanoMenu();
+
+    Main.panel.addToStatusArea('nano-lights', nanoLightsMenu);
+}
+
+/**
+ * This function could be called after the extension is uninstalled,
+ * disabled GNOME Tweaks, when you log out or when the screen locks.
+ *
+ * @method disable
+ */
+function disable() {
+
+    nanoLightsMenu.disconnectSignals(true);
+    nanoLightsMenu.destroy();
+}
